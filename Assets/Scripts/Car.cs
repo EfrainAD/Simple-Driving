93f732c7c01115c0a50a66775340ea7a6d09f9a0
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-12-05 00:11:08 -0500

    Add steering to the Car
    1. Add variables for turning speed and direction.
    2. Add turn sectiong with transform.Rotate.
    3. Add a public furnction to change the turning direction value.
    4. Add canvas with left and right side that has a Event Tigger component, that triggers the directiong value.

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    [SerializeField] private float speed = 10f;
-    [SerializeField] private float accelerate = 0.1f;
+    [SerializeField] private float speed;
+    [SerializeField] private float accelerate;
+    [SerializeField] private float turnSpeed;
     
+    private int turnDirection;
+
     void Update()
     {
+        transform.Rotate(0f, turnDirection * turnSpeed * Time.deltaTime, 0f);
+
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
             speed += (accelerate * Time.deltaTime);
+    }
+    
+    public void Steer(int direction)
+    {
+        turnDirection = direction;
+        Debug.Log("dir " + direction);
     }
 }
