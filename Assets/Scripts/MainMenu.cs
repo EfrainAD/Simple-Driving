c8bddf7e618ce6fd6d0019857dcb3acefba012b5
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-12-08 00:41:51 -0500

    Added engergy to make a player timeout after they played a set number of games

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,13 +8,70 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private TMP_Text highScoreText;
-    private string highScoreKey = ScoreHandler.highScoreKey;
+    private const string highScoreKey = ScoreHandler.highScoreKey;
+
+    [SerializeField] private int maxEnergy;
+    [SerializeField] private float energyRechargeDuration;
+    
+    [SerializeField] private TMP_Text playButton;
+
+    
+
+    private const string energyKey = "Energy";
+    private const string energyRechargeTimeKey = "EnergyRechargeTime";
+
     private void Start() {
         int highScore = PlayerPrefs.GetInt(highScoreKey);
 
         highScoreText.text = $"High Score: {highScore}";
+
+        int energy = PlayerPrefs.GetInt(energyKey, maxEnergy);
+
+        playButton.text = $"{playButton.text} ({energy})";
     }
     public void Play() {
-        SceneManager.LoadScene("Scene_Game");
+        int energy = PlayerPrefs.GetInt(energyKey, 3);
+
+        if (energy > 0)
+        {
+            energy--;
+            if (energy == 0)
+            {
+                DateTime newRecharge = DateTime.Now.AddMinutes(energyRechargeDuration);
+
+                PlayerPrefs.SetString(energyRechargeTimeKey, newRecharge.ToString());
+            }
+            
+            PlayerPrefs.SetInt(energyKey, energy);
+            
+            SceneManager.LoadScene("Scene_Game");
+        }
+        else
+        {
+            if (isEnergyRecharged())
+            {
+                PlayerPrefs.SetInt(energyKey, maxEnergy);
+                playButton.text = $"Play! ({maxEnergy})";
+            }
+        }
+    }
+    private bool isEnergyRecharged()
+    {
+        string energyRechargeTime = PlayerPrefs.GetString(energyRechargeTimeKey, String.Empty);
+            
+            Debug.Log("energyRechargeTime: " + energyRechargeTime);
+
+            if (energyRechargeTime == String.Empty){return false;}
+
+            DateTime rechangeTime = DateTime.Parse(energyRechargeTime);
+
+            if (DateTime.Now > rechangeTime)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
     }
 }
